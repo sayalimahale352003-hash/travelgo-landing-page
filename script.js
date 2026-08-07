1703984a8dbcfc517425230d46596f9b1e3fd963
Sayali Yashwant Mahale <sayalimahale352003@gmail.com>
2026-08-07 18:28:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
